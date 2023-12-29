dcc82abf1e27ba5339d7d2137aa2c6db1cd8faa2
Papagex <geeksgeckosandgoodvibes@gmail.com>
2023-12-29 01:41:11 -0500

    made program multithreading

---
+++ src/stats.rs
@@ -0,0 +1,23 @@
+use crossbeam::channel::Receiver;
+use std::io::Result;
+
+
+pub fn stats_loop(
+    silent: bool,
+    stats_rx: Receiver<usize>) -> Result<()> {
+    let mut total_bytes = 0;
+    loop {
+        let num_bytes= stats_rx.recv().unwrap();
+        total_bytes += num_bytes;
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+        if num_bytes == 0 {
+            break;
+        }
+    }
+    if !silent {
+        eprintln!();
+    }
+    Ok(())
+}
